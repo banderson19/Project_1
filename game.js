7489812f4ed3545e88ef3c4f1431d3bf1a0eefdf
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-03-03 22:54:55 -0700

    added commented code for calling diff pages from rmapi

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
 const textElement = document.getElementById("text")
 const optionsBtnElement = document.getElementById("optionsBtn")
-
+// const chars = await getCharacter()
+// const locations = await getLocation()
+// const episodes = await getEpisode()
+// You can pass a page number to access all the pages inside the info object
+// To know more about the info object and pagination, check
+// https://rickandmortyapi.com/documentation/#info-and-pagination
+// const moreChars = await getCharacter({ page: 2 })
+// const tinyRick = require('rickmortyapi')
 let state = {}
 
 function takeOff() {
@@ -64,7 +71,7 @@ const textNodes = [
             }
         ]
     },
-
+    
     {
         id: 3,
         text: "Citadel of Ricks",
