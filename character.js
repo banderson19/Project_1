f2209df647bdd958d4c8aa26b46e1696878c002c
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-03-10 14:47:01 -0700

    displaying names, no images but that's okay.  will keep trying.

--- character.js
+++ character.js
@@ -1,8 +1,41 @@
-// const chars = await getCharacter()
-// const locations = getLocation()
-// const episodes = await getEpisode()
-// You can pass a page number to access all the pages inside the info object
-// To know more about the info object and pagination, check
-// https://rickandmortyapi.com/documentation/#info-and-pagination
-// const moreChars = await getCharacter({ page: 2 })
-// const tinyRick = require('rickmortyapi')
+// function myAsyncFunction() {
+//   return myAPICall('https://rickandmortyapi.com/api/character')
+//     .then(function (data) {
+//        let result = 2 + 2;
+//        return data;
+//     })
+//     .catch(function (ex) {
+//         return ex;
+//     });
+// // }
+// const API = "https://rickandmortyapi.com/api/character";
+// // const response = await fetch(resource [options]);
+// // const characters = await getCharacter()
+// // const locations = await getLocation()
+// // const episodes = await getEpisode()
+// function getChars(){
+//   fetch(API)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const Names = data.results;
+//     const Image = Names.map((x) => x.image);
+//     let template = "";
+//     Image.forEach((image) => {
+//       template += `<img src="${image}"></image>`;
+//     });
+//     var container = document.querySelector("#div");
+//     container.innerHTML = template;
+//   });
+// }
+
+fetch("https://rickandmortyapi.com/api/character/")
+   .then(response => response.json())
+   .then(characters => showCharacters(characters.results));
+   showCharacters = characters => {
+    const charactersDiv = document.querySelector("#characters");
+    characters.forEach(character => {
+      const characterElement = document.createElement("p");
+      characterElement.innerText = `Character Name: ${character.name}`;
+      charactersDiv.append(characterElement);
+    });
+  }
