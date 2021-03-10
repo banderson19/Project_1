95fa692426b4d9e3ff32439d14cc2dff60a2cb94
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-03-09 23:44:25 -0700

    added modals

--- shwifty.js
+++ shwifty.js
@@ -1,3 +1,29 @@
+var modal = document.getElementById("madLibsModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log('open');
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
 let url = fetch("https://rickandmortyapi.com/api/character/252");
 fetch("https://rickandmortyapi.com/api/character/252")
    .then(response => response.json())
