16b3f46d46a7494c366d478fdadcb6fe29f9c8bb
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-03-09 17:16:02 -0700

    edits/format documents

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const textElement = document.getElementById("text")
-const optionsBtnElement = document.getElementById("optionsBtn")
+const textElement = document.getElementById("text");
+const optionsBtnElement = document.getElementById("optionsBtn");
 // const chars = await getCharacter()
 // const locations = getLocation()
 // const episodes = await getEpisode()
@@ -8,96 +8,91 @@ const optionsBtnElement = document.getElementById("optionsBtn")
 // https://rickandmortyapi.com/documentation/#info-and-pagination
 // const moreChars = await getCharacter({ page: 2 })
 // const tinyRick = require('rickmortyapi')
-let state = {}
+let state = {};
 
 function takeOff() {
-    state = {}
-    showTextNode(1)
-
+  state = {};
+  showTextNode(1);
 }
-function showTextNode(textNodeIndex)
-{
-    const textNode =textNodes.find(textNode => textNode.id === textNodeIndex)
-    textElement.innerText = textNode.text
-    while (optionsBtnElement.firstChild) {
-        optionsBtnElement.removeChild(optionsBtnElement.firstChild)
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
+  textElement.innerText = textNode.text;
+  while (optionsBtnElement.firstChild) {
+    optionsBtnElement.removeChild(optionsBtnElement.firstChild);
+  }
+  textNode.options.forEach((option) => {
+    if (showOption(option)) {
+      const button = document.createElement("button");
+      button.innerText = option.text;
+      button.classList.add("btn");
+      button.classList.add("btn-go");
+      button.addEventListener("click", () => selectOption(option));
+      optionsBtnElement.appendChild(button);
     }
-    textNode.options.forEach(option => {
-        if (showOption(option)) {
-            const button = document.createElement('button')
-            button.innerText = option.text
-            button.classList.add('btn')
-            button.classList.add('btn-go')
-            button.addEventListener("click", () => selectOption(option))
-            optionsBtnElement.appendChild(button)
-        }
-    })
+  });
 }
-function showOption(option)
-{
-    return option.requiredState == null || option.requiredState(state)
-
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state);
 }
-function selectOption(option)
-{
-    const nextTextNodeId = option.nextText
-    state = Object.assign(state, option.setState)
-    showTextNode(nextTextNodeId)
+function selectOption(option) {
+  const nextTextNodeId = option.nextText;
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNodeId);
 }
 const textNodes = [
-    {
-        id: 1,
-        text: "Beaker of Genetic Monstrosities",
-        options: [
-            {
-                text: "Let's go.",
-                nextText: 2
-            },
-        ]
-    },
-    {
-        id: 2,
-        text: "You are walking along and come across a stranger. You must make a trade, the beaker or the ship.",
-        options: [
-            {
-                text: 'SpaceShip',
-                requiredState: (currentState) => currentState.SpaceShip,
-                setState: { SpaceShip: false, beaker: true },
-                nextText: 2
-            },
-            {
-                text: "Spaceship",
-                        nextText: 2
-                    },
-                    {
-                        text: "Beaker",
-                        setState: {beaker: true },
-                        nextText: 3
-                    }
-                ]
-            },
-          {
-            id: 3,
-            text: "Do You Choose to Enter?",
-            options: [
-                {
-                    requiredState: (currentState) => currentState.locationElement,
-                    setState: { Enter: true },
-                    nextText: 4
-                },
-                {
-                    text: "Citadel of Ricks",
-                    nextText: 5
-                },
-            ]
-          },
-]
-                //    [ {
-                //         window.onload = function(){
-                //         document.getElementById("btn-go").onclick= function() {
-                //             var start= prompt("Enter the Citadel");
-                // };
-                // },
-  
+  {
+    id: 1,
+    text: "Beaker of Genetic Monstrosities",
+    options: [
+      {
+        text: "Let's go.",
+        nextText: 2,
+      },
+    ],
+  },
+  {
+    id: 2,
+    text:
+      "You are walking along and come across a stranger. You must make a trade, the beaker or the ship.",
+    options: [
+      {
+        text: "SpaceShip",
+        requiredState: (currentState) => currentState.SpaceShip,
+        setState: { SpaceShip: false, beaker: true },
+        nextText: 2,
+      },
+      {
+        text: "Spaceship",
+        nextText: 2,
+      },
+      {
+        text: "Beaker",
+        setState: { beaker: true },
+        nextText: 3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    text: "Do You Choose to Enter?",
+    options: [
+      {
+        requiredState: (currentState) => currentState.locationElement,
+        setState: { Enter: true },
+        nextText: 4,
+      },
+      {
+        text: "Citadel of Ricks",
+        nextText: 5,
+      },
+    ],
+  },
+];
+//    [ {
+//         window.onload = function(){
+//         document.getElementById("btn-go").onclick= function() {
+//             var start= prompt("Enter the Citadel");
+// };
+// },
 
-takeOff()
+takeOff();
--- nasa.js
+++ nasa.js
@@ -1,39 +1,28 @@
 $.ajax({
+  let: (url =
+    "https://api.nasa.gov/planetary/apod?api_key=ult6atambzs8D68rYchqcYlhYL26w5ohzCPek2Zq&date=1995-06-16"),
 
-    let: url ="https://api.nasa.gov/planetary/apod?api_key=ult6atambzs8D68rYchqcYlhYL26w5ohzCPek2Zq&date=1995-06-16",
-    
-    url: url,
-    
-    success: function success(result)
-    
-    {
-    
+  url: url,
+
+  success: function success(result) {
     //Sometimes they post video too//
-    
-    if(result.media_type == "video") {
-    
-    $("#apod_img_ofDay").css("display", "none");
-    
-    $("#apod_vid_id").attr("src", result.url);
-    
+
+    if (result.media_type == "video") {
+      $("#apod_img_ofDay").css("display", "none");
+
+      $("#apod_vid_id").attr("src", result.url);
+    } else {
+      $("#apod_vid_id").css("display", "none");
+
+      $("#apod_img_ofDay").attr("src", result.url);
     }
-    
-    else {
-    
-    $("#apod_vid_id").css("display", "none");
-    
-    $("#apod_img_ofDay").attr("src", result.url);
-    
-    }
-    
+
     $("#apod_explanation").text(result.explanation);
-    
+
     $("#apod_title").text(result.title);
-    
-    }
-    
-    });
-    // var req = new XMLHttpRequest();
+  },
+});
+// var req = new XMLHttpRequest();
 // var url = "https://api.nasa.gov/planetary/apod?api_key=GL6eDBbPb6LJMU9IV7RDeuyhSIETi8f1RoewVf1K&date=1995-06-16";
 // var api_key_mobile = "ult6atambzs8D68rYchqcYlhYL26w5ohzCPek2Zq";
 // var api_key = "GL6eDBbPb6LJMU9IV7RDeuyhSIETi8f1RoewVf1K";
